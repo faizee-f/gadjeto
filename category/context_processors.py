d1e6214af535d6ef24b6f7a6d388fd85e901286c
faizee-f <mfayisam@gmail.com>
2021-12-17 20:38:22 +0530

    Cart updated

--- category/context_processors.py
+++ category/context_processors.py
@@ -1,6 +1,11 @@
+from vendors.models import Vendors
 from .models import category
 
 
 def menu_links(request):
     links= category.objects.all()
     return dict(links=links)
+
+def brand_link(request):
+    brand_link=Vendors.objects.all()
+    return dict(brand_link=brand_link)
